d4fb7cf3eaecbcb42db9e7b1b83ea1c6f669244f
My Name <fesz99@naver.com>
2024-07-10 18:42:00 +0900

    0710 boardlist

---
+++ p0710_05/src/main/java/com/java/mapper/BoardMapper.java
@@ -0,0 +1,13 @@
+package com.java.mapper;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.java.dto.BoardDto;
+
+@Mapper
+public interface BoardMapper {
+	
+	ArrayList<BoardDto> selectList();
+}
---
+++ p0710_05/src/main/java/com/java/mapper/MemberMapper.java
@@ -0,0 +1,11 @@
+package com.java.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.java.dto.MemberDto;
+
+@Mapper
+public interface MemberMapper {
+	
+	MemberDto selectLogin(MemberDto mdto);
+}
